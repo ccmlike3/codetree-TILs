93e7d6a72e5976e7894cc491d9294205c86567e2
ccmlike3 <ccmlike0@gmail.com>
2024-08-04 22:35:16 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240804/원소 10개의 합/sum-of-10-elements.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int sum;
+
+    sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &n);
+        sum += n;
+    }
+    printf("%d", sum);
+    return 0;
+}
